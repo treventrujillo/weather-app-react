a501e5494f5c48d1c78953cae1a656d336aa3d07
Treven Trujillo <ttrujillo@406Partners.Local>
2018-08-07 13:00:07 -0600

    init commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div id="app">
+        Hello
+      </div>
+    );
+  }
+}
+
+export default App;
